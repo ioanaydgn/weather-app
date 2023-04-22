24f2254727a07b94a4c8668cc9ba20a61cc9cefd
Ioan Mert Aydogan <mertaydogn0@gmail.com>
2023-04-22 13:19:43 +0300

    m

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": 'e0cb439ce7mshff3aa3c3b4b1e68p1fd6b5jsnc9f6a6c970ab',
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "f4e3a269bfff3395cd3f93121829ce3f"; // enter your key from openweather API
